d09a6f5c53f03497d49c7176c016e0d66d6b9452
jiayiNotFound <jiayiwyb@gmail.com>
2023-04-11 16:44:12 +0800

    update
    update

--- CA_ShoppingWebsite/Controllers/HomeController.cs
+++ CA_ShoppingWebsite/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CA_ShoppingWebsite.Models;
+using MySql.Data.MySqlClient;
 
 namespace CA_ShoppingWebsite.Controllers;
 
@@ -15,9 +16,40 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        Search();
         return View();
     }
+    [Route("search")]
+    public User Search() {
+        User user = new User();
+        string connStr = "server=2016110.lfidc.net;user=mysql7402439;database=mysql7402439_db;port=3306;password=soLjM9NJzO;Connect Timeout=60";
+        MySqlConnection conn = new MySqlConnection(connStr);
+      
+        try
+        {
+            Console.WriteLine("Connecting to MySQL...");
+            conn.Open();
 
+            string sql = "SELECT * FROM wp_users";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+            }
+            rdr.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+        conn.Close();
+        Console.WriteLine("Done.");
+        return user;
+
+    }
     public IActionResult Privacy()
     {
         return View();
--- CA_ShoppingWebsite/Models/User.cs
+++ CA_ShoppingWebsite/Models/User.cs
@@ -3,9 +3,12 @@ namespace CA_ShoppingWebsite.Models
 {
 	public class User
 	{
-		public User()
-		{
-		}
+	
+        public string? ID { get; set; }
+        public string? Username { get; set; }
+        public List<Products>? Car { get; set; }
+        public List<Products>? History { get; set; }
+  
 	}
 }
 
