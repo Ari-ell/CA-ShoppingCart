5e660e0996e3781e029f3a4a29a2bad90e25f181
feroz003 <feroz1996.work@gmail.com>
2023-04-11 17:35:04 +0800

    Add Data folder for ConnectionString
    Added folder to contain connectionString for mySql database access

---
+++ CA_ShoppingWebsite/data/data.cs
@@ -0,0 +1,6 @@
+namespace CA_ShoppingWebsite.Controllers;
+
+public class data {
+
+    public static string connectionString = "Server=localhost; userid=root; password=password; port=3306; Database=CAdb";
+}
