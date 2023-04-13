752a3a7a0fef077d76ca9e6debed1263a3e478e7
feroz003 <feroz1996.work@gmail.com>
2023-04-13 17:05:36 +0800

    Update PurchasesController.cs

--- CA_ShoppingWebsite/Controllers/PurchasesController.cs
+++ CA_ShoppingWebsite/Controllers/PurchasesController.cs
@@ -6,15 +6,29 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace CA_ShoppingWebsite.Controllers
+namespace CA_ShoppingWebsite.Controllers;
+
+public class PurchasesController : Controller
 {
-    public class PurchasesController : Controller
+    public IActionResult Index()
     {
-        
-        public IActionResult Index()
+        string checkUser = Request.Cookies["userId"];
+        if (checkUser != null)
         {
-            return View();
+            var userId = Convert.ToInt32(checkUser);
+            var myPurchases = Data.PurchaseData.GetPurchaseOrders(userId);
+            var myActivationCodes = Data.PurchaseData.GetActivationCodes(userId);
+            var myProducts = Data.ProductData.GetProductDetails(userId);
+            var myReviews = Data.ReviewData.GetUserReviews(userId);
+
+
+
+            // If user edits or adds a review
+            //      SetReviews();
         }
+        // When user accesses the Purchases Controller
+        // Will see all past purchases and be able to set reviews
+        return View();
     }
 }
 
