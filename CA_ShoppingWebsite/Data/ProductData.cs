cc8bb66e7b182644c8016699912c936da88059e3
feroz003 <feroz1996.work@gmail.com>
2023-04-13 16:14:51 +0800

    Update ProductData.cs

--- CA_ShoppingWebsite/Data/ProductData.cs
+++ CA_ShoppingWebsite/Data/ProductData.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CA_ShoppingWebsite.Models;
 using MySql.Data.MySqlClient;
 
 namespace CA_ShoppingWebsite.Data;
@@ -14,5 +15,35 @@ public class ProductData
 	{
 		return null;
 	}
+
+	public static List<Models.Product>? GetPurchasedProductDetails(Models.User userId)
+	{
+        var products = new List<Models.Product>();
+        using (var conn = new MySqlConnection(data.cloudDB))
+        {
+            conn.Open();
+            string sql = @"SELECT Name, Description, Img
+                        FROM Product, PurchaseOrder
+                        WHERE Product.ProductId IN
+                        (SELECT ProductId 
+                        FROM PurchaseOrder
+                        WHERE UserId = " + userId;
+            var cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var product = new Models.Product
+                {
+                    Name = (string)reader["Name"],
+                    Description = (string)reader["Description"],
+                    Img = (string)reader["Img"],
+                    Price = (double)reader["Price"],
+                };
+                products.Add(product);
+            }
+            conn.Close();
+        }
+        return products;
+    }
 }
 
