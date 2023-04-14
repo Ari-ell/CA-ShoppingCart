333c49ebf7dc47e21f1b517d56183d86b6f523a1
Ariel <50404812+Ari-ell@users.noreply.github.com>
2023-04-14 16:52:37 +0800

    cart and cartItem models

--- CA_ShoppingWebsite/Models/Cart.cs
+++ CA_ShoppingWebsite/Models/Cart.cs
@@ -3,8 +3,7 @@ namespace CA_ShoppingWebsite.Models;
 
 public class Cart
 {
+	public int? CartId { get; set; }
 	public int? UserId { get; set; }
-	public int? ProductId { get; set; }
-	public int? ProductQty { get; set; }
 }
 
